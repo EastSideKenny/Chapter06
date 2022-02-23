118bb4db765c7ffbbe45207cb76b1d65682c62db
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-23 21:15:36 +0100

    static operator

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -12,6 +12,9 @@ baby1.Name = "Gary";
 // call static method
 Person baby2 = Person.Procreate(harry, jill);
 
+// call an operator
+Person baby3 = harry * mary;
+
 WriteLine($"{harry.Name} has {harry.Children.Count} children.");
 WriteLine($"{mary.Name} has {mary.Children.Count} children.");
 WriteLine($"{jill.Name} has {jill.Children.Count} children.");
@@ -19,3 +22,4 @@ WriteLine(
     format: "{0}'s first child is named \"{1}\".",
     arg0: harry.Name,
     arg1: harry.Children[0].Name);
+
