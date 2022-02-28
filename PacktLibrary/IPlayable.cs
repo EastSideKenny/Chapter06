9f582999c116494b9b606e2cdedb2cd66e8a0c37
Jérôme JACOB <jerome56jj@hotmail.com>
2022-02-28 13:20:21 +0100

    Interfaces , Classes and Structs

---
+++ PacktLibrary/IPlayable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public interface IPlayable
+    {
+        void Play();
+        void Pause();
+
+        void Stop() // Default interface implementation
+        {
+            WriteLine("Default implementation of Stop.");
+        }
+    }
+}
--- PacktLibrary/Person.cs
+++ PacktLibrary/Person.cs
@@ -2,7 +2,7 @@
 
 namespace Packt.Shared;
 
-public class Person : object
+public class Person : object, IComparable<Person>
 {
     // fields
     public string? Name; // ? allows null
@@ -93,5 +93,11 @@ public class Person : object
                 Test.Invoke(this, EventArgs.Empty);
             }
         }
+    }
+
+    public int CompareTo(Person? other)
+    {
+        if (Name is null) return 0;
+        return Name.CompareTo(other?.Name);
     }
 }
