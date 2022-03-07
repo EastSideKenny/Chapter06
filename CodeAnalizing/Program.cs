e6754158423bb86f4defa91be2425da89dfc7f13
Jerome Jacob <Jerome56jj@hotmail.com>
2022-03-07 16:02:09 +0100

    stylecop

--- CodeAnalizing/Program.cs
+++ CodeAnalizing/Program.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Packt">
+// Copyright (c) Packt. All rights reserved.
+// </copyright>
 
 namespace CodeAnalizing
 {
-    internal class Program
+    using System;
+
+    /// <summary>
+    /// The main class for this console app.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry poinbt for this console app.
+        /// </summary>
+        /// <param name="args"> A string array of arguments passed to the console app.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
